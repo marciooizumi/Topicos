07611db5283a91c0559db7d26327019b2191dcb7
Marcio <marciooizumi@hotmail.com>
2016-08-13 16:02:38 -0300

    Testando projeto com jsf - dava erro por conta da versão da dependencia do JSF - Agora roda sem problemas

---
+++ TestandoJSF2/src/main/java/com/oizumi/controller/PegaPreco.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.oizumi.controller;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author Márcio
+ */
+@ManagedBean
+public class PegaPreco {
+    public String getPreco(){
+        return "um e quarenta";
+    }
+}
